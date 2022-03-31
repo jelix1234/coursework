3dc4ffde0e34892b462bc4d7e464987c91820727
jelix1234 <102748566+jelix1234@users.noreply.github.com>
2022-03-31 19:03:48 +0100

    Update task_4A.py

--- task_4A.py
+++ task_4A.py
@@ -1,8 +1,8 @@
 import random
-
+# open text file a,txt from the file directory which incude random interger list
 file1 = open("A.txt", "w")
-for i in range (100):
-  number = str(random.randint(0,100))
+for i in range (500):
+  number = str(random.randint(0,500))
   file1.write(number)
   file1.write("\n")
 file1.close
